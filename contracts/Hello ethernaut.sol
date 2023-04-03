f1ed3309fed231d01c572659a516ccee1572d188
Mahmoud Mourad <mahmoud.mourad.dev@gmail.com>
2023-04-03 03:04:14 -0700

    hello

---
+++ contracts/Hello ethernaut.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Instance {
+
+  string public password;
+  uint8 public infoNum = 42;
+  string public theMethodName = 'The method name is method7123949.';
+  bool private cleared = false;
+
+  // constructor
+  constructor(string memory _password) {
+    password = _password;
+  }
+
+  function info() public pure returns (string memory) {
+    return 'You will find what you need in info1().';
+  }
+
+  function info1() public pure returns (string memory) {
+    return 'Try info2(), but with "hello" as a parameter.';
+  }
+
+  function info2(string memory param) public pure returns (string memory) {
+    if(keccak256(abi.encodePacked(param)) == keccak256(abi.encodePacked('hello'))) {
+      return 'The property infoNum holds the number of the next info method to call.';
+    }
+    return 'Wrong parameter.';
+  }
